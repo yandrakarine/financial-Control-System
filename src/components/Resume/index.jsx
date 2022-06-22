7c6d59f4423d02187179ca41402e4f117583455e
yandrakarine <yandrakarine.dev@outlook.com>
2022-06-22 16:33:33 -0300

    add ResumeItem component on Resume component

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import ResumeItem from '../ResumeItem'
 import * as C from "./styles"
+import {
+  FaRegArrowAltCircleUp,
+  FaRegArrowAltCircleDown,
+  FaDollarSign,
+} from "react-icons/fa"
 
 const Resume = () => {
   return (
-    <C.Container>CARDS</C.Container>
+    <C.Container>
+      <ResumeItem title="Entradas" Icon={FaRegArrowAltCircleUp} value="1000" />
+      <ResumeItem title="Saídas" Icon={FaRegArrowAltCircleDown} value="1000" />
+      <ResumeItem title="Total" Icon={FaDollarSign} value="1000" />
+    </C.Container>
   )
 }
 
