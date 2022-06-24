4329080347aef041b0a93ebf1c5225e31d4ebc93
yandrakarine <yandrakarine.dev@outlook.com>
2022-06-24 10:13:13 -0300

    add Form Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Form from './components/Form'
 import Header from './components/Header'
 import Resume from './components/Resume'
 import GlobalStyle from './styles/global'
@@ -6,8 +7,9 @@ import GlobalStyle from './styles/global'
 const App = () => {
   return (
     <>
-      <Header/>
-      <Resume/>
+      <Header />
+      <Resume />
+      <Form />
       <GlobalStyle />
     </>
   )
